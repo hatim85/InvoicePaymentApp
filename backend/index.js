68cb6c0ca624daefeb9d298055056407c0ac7dc8
hatim85 <devconnect.hatim@gmail.com>
2024-12-05 16:18:41 +0530

    Corrected port

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 connectDB();
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // const provider = new ethers.JsonRpcProvider(ALCHEMY_API_KEY);
 
